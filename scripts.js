172be44cbce86275c5291b3d3db464e938dbb5bd
AJSevillano <correosevi@gmail.com>
2020-03-31 16:05:35 +0100

    Minor fixes

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const app = document.getElementById("root");
 
 const logo = document.createElement("img");
-logo.src = "logo.png";
+logo.src = "img/logo.png";
 
 const DescriptionCharactersLimit = 300;
 
